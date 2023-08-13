faee8e82da749e2d49dd6511ab19674865da26c6
Vishnu Vardan <coolboyvishnuvardan@gmail.com>
2023-08-13 16:54:02 +0530

    setting start and end points

--- Assets/Scripts/FIndPathAStar.cs
+++ Assets/Scripts/FIndPathAStar.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using Unity.Mathematics;
 
 public class PathMarker
 {
@@ -52,15 +53,53 @@ public class FIndPathAStar : MonoBehaviour
     public GameObject start;
     public GameObject end;
     public GameObject pathP;
-    // Start is called before the first frame update
+    
+    PathMarker goalNode;
+    PathMarker startNode;
+
+    PathMarker lastPos;
+
+    bool done = false;
+
+    void removeAllMarkers()
+    {
+        GameObject[] markers = GameObject.FindGameObjectsWithTag("marker");
+        foreach(GameObject marker in markers)
+            Destroy(marker);
+    }
+
+
+    void BeginSearch()
+    {
+        done = false;
+        removeAllMarkers();
+
+        List<MapLocation> locations = new List<MapLocation>();
+        for(int z = 1; z < maze.depth - 1 ; z++)
+            for(int x = 1; x < maze.width - 1 ; x++)
+            {
+                if(maze.map[x,z] != 1)
+                    locations.Add(new MapLocation(x, z));
+            }
+        locations.Shuffle();
+
+        Vector3 startLocation = new Vector3(locations[0].x * maze.scale, 0f, locations[0].z * maze.scale);
+        startNode = new PathMarker(new MapLocation(locations[0].x, locations[0].z) , 0 , 0 , 0  ,Instantiate(start, startLocation, quaternion.identity), null);
+        
+        Vector3 goalLocation = new Vector3(locations[1].x * maze.scale, 0f, locations[1].z * maze.scale);
+        goalNode = new PathMarker(new MapLocation(locations[1].x, locations[1].z) , 0 , 0 , 0  ,Instantiate(end, goalLocation, quaternion.identity), null);
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.P))
+            BeginSearch();
         
     }
 }
